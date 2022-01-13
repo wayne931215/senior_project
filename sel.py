1f305aef733ffab9bfeee846e4e7347e1d6b6682
wayne931215 <75062462+wayne931215@users.noreply.github.com>
2022-01-13 15:00:12 +0800

    selection

---
+++ sel.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random as rd
+import ancestor_map_generating as gen
+
+def select():
+    pos = []
+    s = 0
+    for i in range(gen.num):
+        s += gen.val[i]
+        pos.append(s)
+        
+    a = rd.random(0, s)
+    l = 0
+    r = gen.num
+    m = (l + r) // 2
+
+    while l < r:
+        if pos[m] >= a:
+            r = m
+        else:
+            l = m + 1
+
+    return l
+
