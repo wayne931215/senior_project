ca13fa8aafd8bc4ee9698ea7e9e8929f08d0c1b3
wayne931215 <75062462+wayne931215@users.noreply.github.com>
2022-05-15 17:23:41 +0800

    Update var.py

--- var.py
+++ var.py
@@ -7,12 +7,19 @@ mrate = 1 #mutation rate
 arate = 0.5 #ancestor rate
 
 def print_maze(maze):
+    f = open('output.txt', 'w')
+    f2 = open('output2.txt', 'w')
+
     for i in range(1, SZ + 1):
         for j in range(1, SZ + 1):
             for k in range(1, SZ + 1):
-                print(maze[i][j][k], end=' ')
-            print()
-        print()
+                print("grid[", (i-1) * SZ * SZ + (j-1) * SZ + (k-1), "].val =",int(maze[i][j][k]), end=";", file=f)
+                print(int(maze[i][j][k]), end=' ', file=f2)
+            print(file=f2)
+        print(file=f)
+        print(file=f2)
+
+    f.close()
 
 def proper(x, y, z):
     if x < 1 or x > SZ or y < 1 or y > SZ or z < 1 or z > SZ:
